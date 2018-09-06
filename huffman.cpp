20a7980c24daf09ff106419be864da0319ff15f6
Pravesh Tayal <ptayal@gmail.com>
2018-09-06 16:58:03 +0530

    Huffman Next Version

--- huffman.cpp
+++ huffman.cpp
@@ -17,8 +17,8 @@ class node {
 
 class myless {
   public:
-    bool operator()(const pair<int,node*>& p1, const pair<int,node*>& p2) {
-      return p1.first>p2.first;
+    bool operator()(const node* p1, const node* p2) {
+      return p1->freq > p2->freq;
 }
 };
 /* Why is function not working instead of function object
@@ -47,33 +47,31 @@ void huffman(string &data, unordered_map<char, string>& map) {
 
   // Now we need to sort the characters on frequency.
   // Thus we will use min priority queue with pair <frequency, char>
-  priority_queue<pair<int,node*>, vector<pair<int,node*> >, myless> pq;
+  priority_queue<node*, vector<node*>, myless> pq;
 
   // Add all the character nodes to priority queue
   for(unordered_map<char, int>::iterator it = frequency.begin();
       it!=frequency.end(); it++) {
     const pair<char,int>& p1 = *it;
     node *ptr = new node(it->second, it->first);
-    pair<int,node*> p2(it->second, ptr);
-    pq.push(p2);
+    pq.push(ptr);
   }
 
   // Create a tree
   while(pq.size()>=2)
   {
-    pair<int,node*> p1 = pq.top();
+    node* p1 = pq.top();
     pq.pop();
-    pair<int,node*> p2 = pq.top();
+    node* p2 = pq.top();
     pq.pop();
-    node *ptr = new node(p1.first+p2.first, 'I');
-    ptr->left = p1.second;
-    ptr->right = p2.second;
-    pair<int,node*> p3(p1.first+p2.first, ptr);
-    pq.push(p3);
+    node *ptr = new node(p1->freq+p2->freq, 'I');
+    ptr->left = p1;
+    ptr->right = p2;
+    pq.push(ptr);
   }
   // Here size of pq is 1
   // update the map
-  addCode(pq.top().second, " ", map);
+  addCode(pq.top(), " ", map);
 }
 
 // abbcccddddeeeee
